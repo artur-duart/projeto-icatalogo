29a7c281c3f31f785e5ff6f547ddf638686cb752
Artur <arturduartemoraes@gmail.com>
2021-10-07 09:08:27 -0300

    Finalizado editar da pasta 'categorias'

--- categorias/acoes.php
+++ categorias/acoes.php
@@ -65,6 +65,20 @@ switch ($_POST["acao"]) {
         header('location: index.php');
         break;
 
+    case 'editar':
+        // var_dump($_POST);exit;
+        $id = $_POST["id"];
+        $descricao = $_POST["descricao"];
+
+        $sql = "UPDATE tbl_categoria SET descricao = '$descricao' WHERE id = $id";
+        // echo $sql; exit;
+
+        $resultado = mysqli_query($conexao, $sql);
+
+        header('location: index.php');
+
+        break;
+
     default:
         # code...
         break;
