710c1a45df21db19dd199ba47f33b24502f18a46
Artur <arturduartemoraes@gmail.com>
2021-10-08 11:25:22 -0300

    Mudanças não commitadas

--- produtos/acoes.php
+++ produtos/acoes.php
@@ -4,7 +4,7 @@ require("../database/conexao.php");
 
 switch ($_POST["acao"]) {
     case 'inserir':
-        
+
         // TRATAMENTO DA IMAGEM PARA UPLOAD
 
         // RECUPERA O NOME DO ARQUIVO
@@ -19,8 +19,30 @@ switch ($_POST["acao"]) {
         // UPLOAD DO ARQUIVO:
         move_uploaded_file($_FILES["foto"]["tmp_name"], "fotos/$novoNome");
 
+        // INSERÇÃO DE DADOS NA BASE DE DADOS
+
+        // RECEBIMENTO DOS DADOS
+        $descricao = $_POST["descricao"];
+        $peso = $_POST["peso"];
+        $quantidade = $_POST["quantidade"];
+        $cor = $_POST["cor"];
+        $tamanho = $_POST["tamanho"];
+        $valor = $_POST["valor"];
+        $desconto = $_POST["desconto"];
+        $categoriaId = $_POST["categoria"];
+
+        // CRIAÇÃO DA INSTRUÇÃO SQL DE INSERÇÃO:
+        $sql = "INSERT INTO tbl_produto (descricao, peso, quantidade, cor, tamanho, valor, desconto, imagem, categoria_id) 
+        VALUES ('$descricao', $peso, $quantidade, '$cor', '$tamanho', $valor, $desconto, '$novoNome', $categoriaId)";
+
+        // EXECUÇÃO DO SQL DE INSERÇÃO:
+        $resultado = mysqli_query($conexao, $sql);
+
+        // REDIRECIONAR PARA A INDEX:
+        header('location: index.php');
+
         break;
-    
+
     default:
         # code...
         break;
